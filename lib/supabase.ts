9b1ccff3e81b0325a83d02e55cd917192e05efc6
Tongmotor <tongmotor13@gmail.com>
2026-01-19 20:15:31 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 // ค่า URL ที่ได้จาก ID โปรเจกต์ของคุณ
-const supabaseUrl = 'https://idzmwblubxpkblhjasev.supabase.co';
+const supabaseUrl = 'https://rypfkwpbcmdjkvtjcuuy.supabase.co';
 
 // ค่า Publishable key จากรูปภาพที่คุณส่งมา (กดปุ่ม Copy ไอคอนสี่เหลี่ยมซ้อนกัน)
-const supabaseAnonKey = 'sb_publishable_AYaD1Ws1aIQEDNMOF0pOoA_xyI5SUnm'; 
+const supabaseAnonKey = 'sb_publishable_jI52d7UedQH2_xrcEV6VZA_iMiZyuln'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
